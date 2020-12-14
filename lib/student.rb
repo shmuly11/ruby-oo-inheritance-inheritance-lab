f9bd0901edb7f3bf6ea44aafda294bc088094df0
shmuly11 <shmuly11@gmail.com>
2020-12-14 15:15:30 -0500

    commit tree 93ae661cffc25ab829e1c949f553752ed5a85ca0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(idea)
+        @knowledge << idea
+
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
